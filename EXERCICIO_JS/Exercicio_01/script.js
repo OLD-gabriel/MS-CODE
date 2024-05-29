69143b3350d7defb1f45873b16314a35fdca149c
OLD-gabriel <gabrielcirqueira711@gmail.com>
2024-05-28 22:24:20 -0300

    atualizações de algumas funções para jquery

--- EXERCICIO_JS/Exercicio_01/script.js
+++ EXERCICIO_JS/Exercicio_01/script.js
@@ -2,68 +2,74 @@
 //              FEITO POR GABRIEL CIRQUEIRA
 /* ************************************************************ */
 
-        var btnS = document.getElementById("somar");
-        var btnM = document.getElementById("menos");
-        var btnMULT = document.getElementById("MULT");
-        var btnDIV = document.getElementById("DIV");
-        var btnNome = document.getElementById("enviar_nome");
+$(document).ready(function () {
+    var btnS = $("#somar");
+    var btnM = $("#menos");
+    var btnMULT = $("#MULT");
+    var btnDIV = $("#DIV");
+    var btnNome = $("#enviar_nome");
 
-        btnS.addEventListener("click", function () {
-            calcular("somar");
-        });
+    btnS.on("click", function () {
+        calcular("somar");
+    });
 
-        btnM.addEventListener("click", function () {
-            calcular("menos");
-        });
+    btnM.on("click", function () {
+        calcular("menos");
+    });
 
-        btnMULT.addEventListener("click", function () {
-            calcular("MULT");
-        });
+    btnMULT.on("click", function () {
+        calcular("MULT");
+    });
 
-        btnDIV.addEventListener("click", function () {
-            calcular("DIV");
-        });
+    btnDIV.on("click", function () {
+        calcular("DIV");
+    });
 
-        function calcular(operacao) {
-            var number1 = document.getElementById("number1").value;
-            var number2 = document.getElementById("number2").value;
+    function calcular(operacao) {
+        var number1 = $("#number1").val();
+        var number2 = $("#number2").val();
 
-            if (number1 === "" || number2 === "") {
-                window.alert("Preencha os campos corretamente.");
-                return;
-            }
-
-            imprimir_resultado(operacao, number1, number2);
+        if (number1 === "" || number2 === "") {
+            alert("Preencha os campos corretamente.");
+            return;
         }
 
-        function imprimir_resultado(operacao, number1, number2) {
-            var resultado, msg;
+        imprimir_resultado(operacao, number1, number2);
+    }
 
-            number1 = parseFloat(number1);
-            number2 = parseFloat(number2);
+    function imprimir_resultado(operacao, number1, number2) {
+        var resultado, msg;
 
-            if (operacao == "somar") {
-                resultado = number1 + number2;
-                msg = `A SOMA ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
-            } else if (operacao == "menos") {
-                resultado = number1 - number2;
-                msg = `A SUBTRAÇÃO ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
-            } else if (operacao == "MULT") {
-                resultado = number1 * number2;
-                msg = `A MULTIPLICAÇÃO ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
-            } else if (operacao == "DIV") {
-                if (number2 === 0) {
-                    msg = "Não é possível dividir por zero.";
-                } else {
-                    resultado = number1 / number2;
-                    msg = `A DIVISÃO ENTRE ${number1} E ${number2} = <b> ${resultado} </b>`;
-                }
+        number1 = parseFloat(number1);
+        number2 = parseFloat(number2);
+
+        if (operacao == "somar") {
+            resultado = number1 + number2;
+            msg = `A SOMA ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
+        } else if (operacao == "menos") {
+            resultado = number1 - number2;
+            msg = `A SUBTRAÇÃO ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
+        } else if (operacao == "MULT") {
+            resultado = number1 * number2;
+            msg = `A MULTIPLICAÇÃO ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
+        } else if (operacao == "DIV") {
+            if (number2 === 0) {
+                msg = "Não é possível dividir por zero.";
+            } else {
+                resultado = number1 / number2;
+                msg = `A DIVISÃO ENTRE ${number1} E ${number2} = <b>${resultado}</b>`;
             }
-
-            document.getElementById("resultado").innerHTML = msg;
         }
 
-        btnNome.addEventListener("click", function () {
-            var nome = document.getElementById("nome").value;
-            document.getElementById("nome-display").innerText = nome ? `Bem-vindo, ${nome}!` : window.alert("Preencha o capo nome!");
-        });
+        $("#resultado").html(msg);
+    }
+
+    btnNome.on("click", function () {
+        var nome = $("#nome").val();
+        if (nome) {
+            $("#nome-display").text(`Bem-vindo, ${nome}!`);
+        } else {
+            alert("Preencha o campo nome!");
+        }
+    });
+});
